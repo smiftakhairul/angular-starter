2bfd9df9f11041b7f6528367b36c6e144bcc7da1
S M Iftakhairul <s.m.iftakhairul@gmail.com>
2020-03-09 12:50:59 +0600

    understanding of interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,16 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test', // other 2 ways to define -> .app-test, [app-test]
   // templateUrl: './test.component.html',
-  template: `<div>Inline Template of {{ getTitle() }} Component</div>`,
+  template: `
+      <div class="orange">Inline Template of {{ getTitle() }} {{ subTitle }}</div>
+      <div>{{ 2 + 2 }}</div>
+      <div>{{ 'Welcome ' + name }}</div>
+      <div>{{ 'Welcome ' + name.toUpperCase() }}</div>
+      <div>{{ 'Name Length: ' + name.length }}</div>
+      <div>{{ greet() }}</div>
+      <div><a href="{{ siteUrl }}">{{ siteUrl }}</a></div>
+  `,
   // styleUrls: ['./test.component.css'],
   styles: [`
-      div {
+      div.orange {
         color: orange
       }
   `]
 })
 export class TestComponent implements OnInit {
   private title = 'Test';
+  public subTitle = 'Component';
+  public name = "S M Iftakhairul";
+  public siteUrl = window.location.href;
 
   constructor() { }
 
@@ -21,6 +32,10 @@ export class TestComponent implements OnInit {
 
   getTitle() {
     return this.title;
+  }
+
+  greet() {
+    return 'Hello S M Iftakhairul';
   }
 
 }
